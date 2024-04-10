349ec59bab8a336356a43d62e8b87cf0309ca87f
LowkeyCoding <loke0046@gmail.com>
2024-04-10 18:22:09 +0200

    Added samples for azure

--- sample.py
+++ sample.py
@@ -1,20 +1,32 @@
-from qiskit import *
-import numpy as np
-from qiskit_aer import AerSimulator
+from azure.quantum import Workspace
+from azure.quantum.qiskit import AzureQuantumProvider
+from qiskit import QuantumCircuit
 from qiskit.visualization import plot_histogram
+from azure.quantum.qiskit import AzureQuantumProvider
+from qiskit import transpile
+from matplotlib import pyplot
 
-backend = AerSimulator()
-circ = QuantumCircuit(3)
+workspace = Workspace(  
+    resource_id = " ", # Add the resourceID of your workspace
+    location = "" # Add the location of your workspace (for example "westus")
+)
 
-circ.h(0)
-circ.cx(0, 1)
-circ.cx(0, 2)
+provider = AzureQuantumProvider(workspace)
 
-circ.measure_all()
-circ.draw('mpl')
+backend = provider.get_backend("ionq.simulator")
 
-qc_compiled = transpile(circ, backend)
+circuit = QuantumCircuit(3, 3)
+circuit.name = "TEST"
+circuit.h(0)
+circuit.cx(0, 1)
+circuit.cx(1, 2)
+circuit.measure([0,1,2], [0, 1, 2])
+
+circuit.draw('mpl')
+qc_compiled = transpile(circuit, backend)
 job_sim = backend.run(qc_compiled, shots=1024)
 result_sim = job_sim.result()
 counts = result_sim.get_counts(qc_compiled)
+
 plot_histogram(counts)
+pyplot.show()
