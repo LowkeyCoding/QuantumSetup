f6ac919fe3c684bd1c1a465a58eb9faf8dbe0d3d
LowkeyCoding <loke0046@gmail.com>
2024-04-10 18:40:39 +0200

    Added sample with noise

--- sample_with_noise.py
+++ sample_with_noise.py
@@ -1,9 +1,11 @@
+# It is required to install qiskit_ibm_runtime
+
 from qiskit import *
 import numpy as np
 from qiskit_aer import AerSimulator
 from qiskit.visualization import plot_histogram
+from qiskit_ibm_runtime import QiskitRuntimeService
 from matplotlib import pyplot
-
 circ = QuantumCircuit(3)
 
 circ.h(0)
@@ -12,7 +14,11 @@ circ.cx(0, 2)
 
 circ.measure_all()
 circ.draw('mpl')
-backend = AerSimulator()
+
+provider = QiskitRuntimeService()
+backend = provider.get_backend("ibm_kyoto")
+backend = AerSimulator.from_backend(backend)
+
 qc_compiled = transpile(circ, backend)
 job_sim = backend.run(qc_compiled, shots=1024)
 result_sim = job_sim.result()
