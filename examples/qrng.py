ede0c699bef10783f3726270da9f204c667366e8
LowkeyCoding <loke0046@gmail.com>
2024-06-23 18:19:45 +0200

    Added QRNG algorithm

---
+++ examples/qrng.py
@@ -0,0 +1,38 @@
+from azure.quantum import Workspace
+from azure.quantum.qiskit import AzureQuantumProvider
+from matplotlib import pyplot
+from dotenv import load_dotenv
+from qiskit import QuantumCircuit, transpile, QuantumRegister, ClassicalRegister
+from qiskit.visualization import plot_histogram
+import numpy as np
+import os
+
+load_dotenv()
+
+workspace = Workspace(resource_id=os.environ['azure_id'], location=os.environ['azure_location'])
+provider = AzureQuantumProvider(workspace)
+# Selecting a backend
+# Use simulators to test before running it on real hardware.
+backend = provider.get_backend("rigetti.sim.qvm")
+
+num_bits = 4 # number of random bits to generate
+
+qr = QuantumRegister(num_bits, name='qr')
+cr = ClassicalRegister(num_bits, name='cr')
+qrng = QuantumCircuit(qr,cr)
+
+for i in range(num_bits):
+    qrng.h(qr[i])
+
+qrng.measure(qr, cr)
+qrng.draw("mpl")
+
+# Run shor code circuit
+qc_compiled = transpile(qrng, backend)
+job_sim = backend.run(qc_compiled, shots=1024)
+result_sim = job_sim.result()
+counts = result_sim.get_counts(qc_compiled)
+
+# Plot the result
+plot_histogram(counts)
+pyplot.show()
