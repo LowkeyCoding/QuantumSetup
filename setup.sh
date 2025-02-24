8e124d8fec3f9a82eca20379a4a219c051be79e9
LowkeyCoding <loke0046@gmail.com>
2025-02-24 12:35:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ curl -LsSf -o $HOME/.local/bin/autosetup.py https://raw.githubusercontent.com/Lo
 
 # Define the target directory
 TARGET_DIR="$HOME/.local/bin"
-
+TARGET_NAME="qproject"
 # Create the target directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
 
@@ -13,11 +13,15 @@ SCRIPT_CONTENT='#!/bin/sh
 uv run --with "requests,colorama" $HOME/.local/bin/autosetup.py'
 
 # Remove old version of quantum init
-rm $TARGET_DIR/quantum_init
+if [ -e "$TARGET_DIR/$TARGET_NAME" ]; then
+    echo "Removing old version of: $TARGET_DIR/$TARGET_NAME"
+    rm $TARGET_DIR/$TARGET_NAME
+fi
 # Write the content to a new script file in the target directory
-echo "#!/bin/sh" > "$TARGET_DIR/quantum_init"
-echo "uv run --with \"requests,colorama\" --python 3.12.9 $TARGET_DIR/autosetup.py" > "$TARGET_DIR/quantum_init"
+echo "#!/bin/sh" > "$TARGET_DIR/$TARGET_NAME"
+echo "uv run --with \"requests,colorama\" --python 3.12.9 $TARGET_DIR/autosetup.py" > "$TARGET_DIR/qproject"
 # Make the new script executable
-chmod +x "$TARGET_DIR/quantum_init"
+chmod +x "$TARGET_DIR/$TARGET_NAME"
 
-echo "Generated executable script: $TARGET_DIR/quantum_init"
+echo "Installing to $TARGET_DIR"
+echo "  $TARGET_NAME"
